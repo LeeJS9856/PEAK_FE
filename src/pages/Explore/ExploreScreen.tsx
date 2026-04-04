d2e48113452fd9404eb437ff873500cce2747ebb
LeeJS9856 <wnstj1057@naver.com>
2026-04-04 19:52:03 +0900

    feat : react native studio map 설치

--- src/pages/Explore/ExploreScreen.tsx
+++ src/pages/Explore/ExploreScreen.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { NaverMapView } from '@mj-studio/react-native-naver-map';
 
 const ExploreScreen = () => {
   return (
     <View style={styles.container}>
-      
+      <NaverMapView/>
       
     </View>
   );
