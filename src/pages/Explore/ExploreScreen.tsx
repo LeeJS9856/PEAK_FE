6339f41dd6c160673a8e9df01c17bb89624532e0
LeeJS9856 <wnstj1057@naver.com>
2026-04-08 17:48:21 +0900

    feat : webview로 네이버 지도 로드

--- src/pages/Explore/ExploreScreen.tsx
+++ src/pages/Explore/ExploreScreen.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { NaverMapView } from '@mj-studio/react-native-naver-map';
+import React, { useRef } from 'react';
+import { StyleSheet, View, Platform } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 const ExploreScreen = () => {
+  const webViewRef = useRef(null);
+
+  // 안드로이드와 iOS 경로 처리
+  const source = Platform.select({
+    ios: require('./naver_map.html'), // iOS는 파일 상대경로
+    android: { uri: 'file:///android_asset/naver_map.html' }, // 안드로이드는 assets 경로
+  });
+
+  const onMessage = (event: any) => {
+    const data = JSON.parse(event.nativeEvent.data);
+    console.log("선택한 좌표:", data);
+  };
+
   return (
     <View style={styles.container}>
-      <NaverMapView/>
-      
+      <WebView
+        ref={webViewRef}
+        source={source}
+        originWhitelist={['*']}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        allowFileAccessFromFileURLs={true}
+        allowUniversalAccessFromFileURLs={true}
+        allowFileAccess={true}
+        onMessage={onMessage} // 지도에서 보낸 데이터 수신
+        style={{ flex: 1 }}
+      />
     </View>
   );
 };
@@ -14,19 +37,6 @@ const ExploreScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
-    backgroundColor: '#ffffff',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 28,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  subtitle: {
-    fontSize: 14,
-    color: '#666',
   },
 });
 
